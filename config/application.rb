e48603ca79ee31a24ad0cb1de7aa0a415c6f54af
knit tkg <ktakagi@nttpc.co.jp>
2018-02-11 14:09:26 +0000

    revised css files for glyphicons

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Inclone
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # glyphiconsが使っている*.eot *.svg *.ttf *.woffらを precompile の対象に含める
+    config.assets.precompile += %w(*.eot *.svg *.ttf *.woff)
   end
 end
